b9db697aa7541df3a3fc8243946a653a957fefd3
E Camden Fisher <fish@fishnix.net>
2019-12-20 15:26:53 -0500

    add test coverage for orchestrating secrets, update deco for spinupsec (#25)

--- orchestration/repositorycredentials_test.go
+++ orchestration/repositorycredentials_test.go
@@ -1,9 +1,67 @@
 package orchestration
 
 import (
+	"context"
+	"fmt"
+	"reflect"
 	"testing"
+
+	sm "github.com/YaleSpinup/ecs-api/secretsmanager"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/request"
+	"github.com/aws/aws-sdk-go/service/secretsmanager"
 )
 
-func TestProcessRepositoryCredentials(t *testing.T) {
+func (m *mockSMClient) CreateSecretWithContext(ctx context.Context, input *secretsmanager.CreateSecretInput, opts ...request.Option) (*secretsmanager.CreateSecretOutput, error) {
+	if m.err != nil {
+		return nil, m.err
+	}
 
+	if input == nil {
+		return nil, awserr.New(secretsmanager.ErrCodeInvalidRequestException, "invalid input", nil)
+	}
+
+	if aws.StringValue(input.Name) == "" {
+		return nil, awserr.New(secretsmanager.ErrCodeInvalidRequestException, "Name is required", nil)
+	}
+
+	if (input.SecretBinary == nil && input.SecretString == nil) || (input.SecretBinary != nil && input.SecretString != nil) {
+		return nil, awserr.
+			New(secretsmanager.ErrCodeInvalidRequestException, "secret string OR secretbinary is required", nil)
+	}
+
+	arn := fmt.Sprintf("arn:%s", aws.StringValue(input.Name))
+	return &secretsmanager.CreateSecretOutput{
+		ARN:       aws.String(arn),
+		Name:      input.Name,
+		VersionId: aws.String("v1"),
+	}, nil
+}
+
+func TestProcessRepositoryCredentials(t *testing.T) {
+	o := Orchestrator{
+		SecretsManager: sm.SecretsManager{Service: &mockSMClient{t: t}},
+	}
+	out, err := o.processRepositoryCredentials(context.TODO(), &ServiceOrchestrationInput{})
+	if err != nil {
+		t.Errorf("expected nil error for processRepositoryCredentials, got %s", err)
+	}
+
+	if out != nil {
+		t.Errorf("expected nil output for empty repository credentials, got %+v", out)
+	}
+
+	out, err = o.processRepositoryCredentials(context.TODO(), &ServiceOrchestrationInput{
+		TaskDefinition: tdInput,
+		Credentials:    credentialsMapIn,
+	})
+	if err != nil {
+		t.Errorf("expected nil error for processRepositoryCredentials, got %s", err)
+	}
+
+	t.Log("got processRepositoryCredentials response", out)
+	if !reflect.DeepEqual(credentialsMapOut, out) {
+		t.Fatalf("Expected %+v\nGot %+v", credentialsMapOut, out)
+	}
 }
