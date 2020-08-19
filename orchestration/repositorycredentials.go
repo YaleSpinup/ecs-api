cc0d8543a54df2e9cd7f96d3a715115f5ded160b
E Camden Fisher <fish@fishnix.net>
2020-08-18 21:34:32 -0400

    update processRepositoryCredentialsUpdate to work for known cases

--- orchestration/repositorycredentials.go
+++ orchestration/repositorycredentials.go
@@ -2,6 +2,7 @@ package orchestration
 
 import (
 	"context"
+	"errors"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ecs"
@@ -85,80 +86,150 @@ func (o *Orchestrator) processRepositoryCredentials(ctx context.Context, input *
 	return creds, rbfunc, nil
 }
 
-// processRepositoryCredentialsUpdate processes the passed in repository credentials and applies them appropriately.  If the
-// container definition already has credentials set, assume we are updating the credentials in place. In that case, we shouldn't
-// need to set the repository credentials on the input since they are already applied to the container def.  Otherwise, assume
-// the credentials are new (maybe the container def is new too).  In this case, we create the secret and apply the result to the input.
-func (o *Orchestrator) processRepositoryCredentialsUpdate(ctx context.Context, input *ServiceOrchestrationUpdateInput) (map[string]interface{}, error) {
-	if len(input.Credentials) == 0 {
-		log.Debugf("no private repository credentials passed")
-		return nil, nil
+// processRepositoryCredentialsUpdate processes the repository credentials for the container definitions inside the task definition.
+//
+// If the active container definition HAS repostory credentials set
+// ...AND the input has Credentials defined for the container definition
+// ...AND the input has repository credentials set for the container definition
+// ...THEN update the secret at the (active) ARN in place with the given Credentials and apply to the container definition
+//
+// If the active container definition HAS repository credentials set
+// ...AND the input has Credentials defined for the container definition
+// ...AND the input doesn't have repository credentials set for the container definition
+// ...THEN set the input repository credentials to the ARN for the active container definition repository credentials
+// ...AND update the secret at the ARN in place with the given Credentials
+//
+// If the active container definition HAS repository credentials set
+// ...AND the input doesn't have Credentials defined for the container definition
+// ...AND the input has repository credentials set for the container definition
+// ...THEN override the repository credentials with the ARN of the active repository credentials
+//
+// If the active container definition HAS repository credentials set
+// ...AND the input doesn't have repository credentials set
+// ...AND the input doesn't have Credentials defined for the container definition
+// ...THEN delete the secret at the ARN for the active container definition
+//
+// If the active container definition doesn't exist or doesn't have repository credentials set
+// ...AND the input has Credentials defined for the container definition
+// ...AND the input has repository credentials defined for the container definition
+// ...THEN update the secret in place or fail if it doesn't exist
+// Note: (this case shouldn't happen)
+//
+// If the active container doesn't exist or doesn't have repository credentials set
+// ...AND the input has Credentials defined for the container definition
+// ...THEN create a new secret and apply the resulting ARN to the repository credentials for the input
+//
+// If the active container doesn't exist or doesn't have repository credentials set
+// ...AND the input doesn't have Credentials defined for the container definition
+// ...THEN assume public image, no secrets are created, no repository credentials are applied
+//
+func (o *Orchestrator) processRepositoryCredentialsUpdate(ctx context.Context, input *ServiceOrchestrationUpdateInput, active *ServiceOrchestrationUpdateOutput) error {
+	if active == nil || active.TaskDefinition == nil || active.TaskDefinition.ContainerDefinitions == nil {
+		return errors.New("cannot process nil active input")
 	}
+
+	activeRepositoryCredentials := containterDefinitionCredsMap(active.TaskDefinition.ContainerDefinitions)
+	log.Debugf("active repository credentials: %+v", activeRepositoryCredentials)
+
+	inputRepositoryCredentials := containterDefinitionCredsMap(input.TaskDefinition.ContainerDefinitions)
+	log.Debugf("input repository credentials: %+v", inputRepositoryCredentials)
+
+	inputCredentials := input.Credentials
+	log.Debugf("input credentials %+v", inputCredentials)
 
 	client := o.SecretsManager
 	creds := make(map[string]interface{}, len(input.Credentials))
 	for _, cd := range input.TaskDefinition.ContainerDefinitions {
 		containerName := aws.StringValue(cd.Name)
-		log.Debugf("processing container definition %s", containerName)
-		if secret, ok := input.Credentials[containerName]; ok {
-			// if the credentials parameter is specified in the container definition, assume we are updating
-			// the credentia in place.  otherwise, assume we are creating a *new* secret/credential
-			if cd.RepositoryCredentials != nil && cd.RepositoryCredentials.CredentialsParameter != nil {
-				secretArn := cd.RepositoryCredentials.CredentialsParameter
-				log.Infof("updating repository credentials secret '%s' for container definition: %s", containerName, aws.StringValue(secretArn))
+		log.Debugf("processing container definition %s repository credentials", containerName)
 
-				secretUpdate := secretsmanager.PutSecretValueInput{
-					ClientRequestToken: secret.ClientRequestToken,
-					SecretId:           secretArn,
-					SecretString:       secret.SecretString,
-				}
+		activeRepositoryCredential, hasActiveRepositoryCredential := activeRepositoryCredentials[containerName]
+		_, hasInputRepositoryCredential := inputRepositoryCredentials[containerName]
+		inputCredential, hasInputCredential := inputCredentials[containerName]
 
-				out, err := client.UpdateSecret(ctx, &secretUpdate)
-				if err != nil {
-					return nil, err
-				}
-
-				log.Debugf("output: %+v", out)
-				creds[containerName] = out
-			} else {
-				log.Infof("creating new repository credentials secret for container definition: %s", containerName)
-
-				smTags := make([]*secretsmanager.Tag, len(input.TaskDefinition.Tags))
-				for i, t := range input.TaskDefinition.Tags {
-					smTags[i] = &secretsmanager.Tag{Key: t.Key, Value: t.Value}
-				}
-				secret.Tags = smTags
-
-				org := ""
-				if o.Org != "" {
-					org = o.Org + "/"
-				}
-
-				cluster := ""
-				if input.Service != nil && input.Service.Cluster != nil {
-					cluster = aws.StringValue(input.Service.Cluster) + "/"
-				}
-
-				secret.Name = aws.String("spinup/" + org + cluster + aws.StringValue(secret.Name))
-
-				out, err := client.CreateSecret(ctx, secret)
-				if err != nil {
-					return nil, err
-				}
-
-				log.Debugf("output: %+v", out)
-
-				log.Infof("setting repository credentials secret for container definition: %s to %s", containerName, aws.StringValue(out.ARN))
-
-				cd.SetRepositoryCredentials(&ecs.RepositoryCredentials{CredentialsParameter: out.ARN})
-				creds[containerName] = out
+		// if there are active repository credentials and no input repository credentials or input credentials,
+		// delete the secret at the active repository credentials
+		if hasActiveRepositoryCredential && !hasInputRepositoryCredential && !hasInputCredential {
+			log.Warnf("active %s container has repository credentials (%s) but updated definition doesn't, deleting credentials", containerName, activeRepositoryCredential)
+			if _, err := client.DeleteSecret(ctx, activeRepositoryCredential, 0); err != nil {
+				return err
 			}
+			// if there are active repository credentials, set the input repository credentials to the active repository credentials
+		} else if hasActiveRepositoryCredential {
+			log.Debugf("overriding input repository credentials with active repository credentials")
+			cd.RepositoryCredentials = &ecs.RepositoryCredentials{
+				CredentialsParameter: aws.String(activeRepositoryCredential),
+			}
+			hasInputRepositoryCredential = true
+		}
+
+		// if there is an input credential and an input repository credential, update the input repository
+		// credential with the active credential.  else if there's an input credential and no input repository
+		// credential, create a new secret
+		if hasInputCredential && hasInputRepositoryCredential {
+			secretArn := cd.RepositoryCredentials.CredentialsParameter
+
+			log.Infof("updating repository credentials secret '%s' for container definition: %s", containerName, aws.StringValue(secretArn))
+
+			secretUpdate := secretsmanager.PutSecretValueInput{
+				ClientRequestToken: inputCredential.ClientRequestToken,
+				SecretId:           secretArn,
+				SecretString:       inputCredential.SecretString,
+			}
+
+			out, err := client.UpdateSecret(ctx, &secretUpdate)
+			if err != nil {
+				return err
+			}
+
+			log.Debugf("update secret output for %s: %+v", containerName, out)
+
+			creds[containerName] = out
+		} else if hasInputCredential {
+			log.Infof("creating new repository credentials secret for container definition: %s", containerName)
+
+			smTags := make([]*secretsmanager.Tag, len(input.TaskDefinition.Tags))
+			for i, t := range input.TaskDefinition.Tags {
+				smTags[i] = &secretsmanager.Tag{Key: t.Key, Value: t.Value}
+			}
+			inputCredential.Tags = smTags
+
+			org := ""
+			if o.Org != "" {
+				org = o.Org + "/"
+			}
+
+			cluster := ""
+			if input.Service != nil && input.Service.Cluster != nil {
+				cluster = aws.StringValue(input.Service.Cluster) + "/"
+			}
+
+			inputCredential.Name = aws.String("spinup/" + org + cluster + aws.StringValue(inputCredential.Name))
+
+			out, err := client.CreateSecret(ctx, inputCredential)
+			if err != nil {
+				return err
+			}
+
+			log.Debugf("create secret output for %s: %+v", containerName, out)
+
+			log.Infof("setting repository credentials secret for container definition: %s to %s", containerName, aws.StringValue(out.ARN))
+
+			cd.RepositoryCredentials = &ecs.RepositoryCredentials{
+				CredentialsParameter: out.ARN,
+			}
+
+			creds[containerName] = out
 		} else {
-			log.Infof("assuming container definition %s references a public image, no credentials included", containerName)
+			log.Infof("nothing to do for %s", containerName)
 		}
 	}
 
-	return creds, nil
+	log.Debugf("processed update of repository credentials: %+v", creds)
+
+	active.Credentials = creds
+
+	return nil
 }
 
 func (o *Orchestrator) processSecretsmanagerTags(tags []*Tag) []*secretsmanager.Tag {
@@ -172,4 +243,16 @@ func (o *Orchestrator) processSecretsmanagerTags(tags []*Tag) []*secretsmanager.
 	log.Debugf("returning processed secretsmanager tag list %+v", smTags)
 
 	return smTags
+}
+
+func containterDefinitionCredsMap(containerDefinitions []*ecs.ContainerDefinition) map[string]string {
+	creds := map[string]string{}
+	for _, cd := range containerDefinitions {
+		if cd.RepositoryCredentials != nil && cd.RepositoryCredentials.CredentialsParameter != nil {
+			name := aws.StringValue(cd.Name)
+			credsArn := aws.StringValue(cd.RepositoryCredentials.CredentialsParameter)
+			creds[name] = credsArn
+		}
+	}
+	return creds
 }
